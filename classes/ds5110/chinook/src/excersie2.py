e99e7329573d7db94f4ddbcab01406a13df85dca
tom2470 <henehan.t@northeastern.edu>
2023-07-23 20:50:47 -0500

    hi

--- classes/ds5110/chinook/src/excersie2.py
+++ classes/ds5110/chinook/src/excersie2.py
@@ -3,12 +3,40 @@ import sqlite3
 import pandas as pd
 con = sqlite3.connect('data/chinook.db')
 cur = con.cursor()
-df=pd.read_sql_query("SELECT Name FROM Track ORDER BY TrackId LIMIT 5",con)
-#sql="SELECT Name FROM Track over /(Partition BY GenreId ORDER BY TrackId DESC)"
-#df=pd.read_sql_query("SELECT Name FROM Track RANK() OVER(Partition BY GenreId ORDER BY TrackId DESC)RANK",con)
-#print(df)
-#df=pd.read_sql_query("SELECT TrackId ,COUNT(*) AS `num` FROM Invoiceline GROUP BY TrackId",con)
-#,COUNT(*) AS `num` FROM Invoiceline GROUP BY TrackId
-#print(max(df.num))
-#This produces a max of 1 and 2 invoices per country so it is negligible to find the best songs
+def makedict(dbname,mergeobject,bigbdname):
+  df_name=pd.read_sql_query("SELECT {1}, {0} FROM {2}".format(dbname,mergeobject,bigbdname),con)
+  df_name=df_name.set_index(mergeobject, drop=True)
+  mydict=df_name.to_dict()
+  mydict=mydict[dbname]
+  return mydict
+#2.1
+df=pd.read_sql_query("SELECT Invoice.BillingCountry, InvoiceLine.TrackId  FROM InvoiceLine INNER JOIN Invoice ON InvoiceLine.InvoiceId=Invoice.InvoiceId", con)
+df=df['TrackId'].value_counts()
+df=df.reset_index()
+df.columns=['Trackid','counters']
+print(df.sort_values(by=['counters','Trackid'],ascending=[False,True]).head(5))
+#2.2
+df=pd.read_sql_query("SELECT Invoice.BillingCountry, InvoiceLine.TrackId, Track.Name  FROM InvoiceLine INNER JOIN Invoice ON InvoiceLine.InvoiceId=Invoice.InvoiceId INNER JOIN Track ON InvoiceLine.TrackId=Track.TrackId", con)
+df2 = df.groupby(['BillingCountry'])['TrackId'].value_counts()
+df2.groupby(level=[0,1]).nlargest(5)
+country=[]
+trackid=[]
+for i in df2.index:
+  country.append(i[0])
+  trackid.append(i[1])
+values2=df2.tolist()
+df = pd.DataFrame(list(zip(country, trackid, values2)),columns =['country', 'trackid','counters'])
+print(df.sort_values(by=['country','counters','trackid'],ascending=[True,False,True]))
+#2.3
+mydict=makedict('Name','TrackId','Track')
+df['name']=df['trackid'].apply(lambda x: mydict[x])
+print(df[['country', 'name']])
+#2.4
+mydict=makedict('AlbumId','TrackId','Track')
+df['AlbumId']=df['trackid'].apply(lambda x: mydict[x])
+mydict=makedict('ArtistId','AlbumId','Album')
+df['ArtistId']=df['AlbumId'].apply(lambda x: mydict[x])
+mydict=makedict('Name','ArtistId','Artist')
+df['Artist_name']=df['ArtistId'].apply(lambda x: mydict[x])
+print(df[['country', 'name','Artist_name']])
 con.close()
